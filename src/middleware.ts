df7e0d1ce7aebdcb42eb42e3999159a1829a8d75
ratnadeep2k <55341578+Ratnadeep2k@users.noreply.github.com>
2024-06-24 14:45:44 +0530

    chore: Update .env file and add bcryptjs dependency

--- src/middleware.ts
+++ src/middleware.ts
@@ -17,7 +17,7 @@ export async function middleware(request: NextRequest) {
     ){
         return NextResponse.redirect(new URL('/dashboard', request.url))
     }
-  return NextResponse.redirect(new URL('/home', request.url))
+//   return NextResponse.redirect(new URL('/home', request.url))
 }
  
 // See "Matching Paths" below to learn more
