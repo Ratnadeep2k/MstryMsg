3b52dda40bba08da85e9682aafde9227367b4706
ratnadeep2k <55341578+Ratnadeep2k@users.noreply.github.com>
2024-06-25 20:18:44 +0530

    chore: Update .env file and resend API key

--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -8,7 +8,7 @@ export async function sendVerificationEmail(
 ):Promise<ApiResponse>{
     try {
         await resend.emails.send({
-            from: 'you@example.com',
+            from: 'Acme <onboarding@resend.dev>',
             to: email,
             subject: 'Verification Code',
             react: VerificationEmail({username,otp:verifyCode}),
