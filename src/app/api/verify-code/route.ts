6dd104ddb6ad0f9c6dcabfee911ed2b15d35848a
ratnadeep2k <55341578+Ratnadeep2k@users.noreply.github.com>
2024-06-27 23:42:12 +0530

    feat: Add API route for verifying user code

---
+++ src/app/api/verify-code/route.ts
@@ -0,0 +1,62 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+
+export async function POST(request :Request){
+    await dbConnect()
+    try {
+        const {username,code} = await request.json()
+       const decodedUsername =  decodeURIComponent(username)
+       const user = await UserModel.findOne({username :decodedUsername})
+       if(!user){
+              return Response.json({
+                success: false,
+                message: "User not found"
+              },
+              {
+                status: 404
+              }
+              )
+       }
+       const isCodevalid = user.verifyCode === code
+       const isCodeNotExpired = new Date(user.verifyCodeExpiry) > new Date()
+         if(isCodevalid && isCodeNotExpired){
+              user.isVerified = true
+              await user.save()
+              return Response.json({
+                success: true,
+                message: "User Verified"
+              },{
+                status: 200
+              })
+         }
+         else if(!isCodeNotExpired){
+                return Response.json({
+                    success: false,
+                    message: "Code Expired , Sign up again to get a new code "
+                },{
+                    status: 400
+                })
+         }
+         else {
+            return Response.json({
+                success: false,
+                message: "Invalid Code"
+            },{
+                status: 400
+            })
+         }
+
+        
+    } catch (error) {
+        console.log("Error Verifying user",error)
+        return Response.json(
+        {
+            success: false,
+            message: "Error verifying user"
+        },
+        {
+            status: 500
+        }
+    )
+    }
+}
