88fe85719cae3c0b7147b4c904fe58c8977789af
ratnadeep2k <55341578+Ratnadeep2k@users.noreply.github.com>
2024-06-27 23:14:09 +0530

    chore: Update check-username-unique route to include username in response

--- src/app/api/check-username-unique/route.ts
+++ src/app/api/check-username-unique/route.ts
@@ -34,8 +34,21 @@ export async function GET(request : Request){
        }
 
        const {username} = result.data
-       
-
+      const existingVerifiedUser = await UserModel.findOne({username , isVerified:true})
+      if(existingVerifiedUser){
+            return Response.json({
+                success: false,
+                message: "Username already exists"
+            },
+            {
+                status: 409
+            }
+            )
+      }
+        return Response.json({
+            success: true,
+            message: "Username is available"
+        })
         
     } catch (error) {
         console.error("Error checking username",error)
