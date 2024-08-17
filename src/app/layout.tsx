d7ff4ad015bd25d43d187ee923962943b80a8710
ratnadeep2k <55341578+Ratnadeep2k@users.noreply.github.com>
2024-08-17 20:07:55 +0530

    feat: Add @radix-ui/react-alert-dialog dependency and update navbar component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-// import "./globals.css";
+import "./globals.css";
+import AuthProvider from "@/context/AuthProvider";
+import { Toaster } from "@/components/ui/toaster" 
 import Navbar from "@/components/navbar";
  
 const inter = Inter({ subsets: ["latin"] });
@@ -17,12 +19,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-     
+      <AuthProvider>
       <body className={inter.className}>
         <Navbar/>
         {children}
+      <Toaster/>
       </body>
-     
+      </AuthProvider>
     </html>
   );
 }
