281c80b7ad56471d37e30f570d575115d5d534e7
ratnadeep2k <55341578+Ratnadeep2k@users.noreply.github.com>
2024-08-17 20:12:16 +0530

    chore: Add axios dependency and delete message functionality in MessageCard component

--- src/components/MessageCard.tsx
+++ src/components/MessageCard.tsx
@@ -23,6 +23,8 @@ import { Button } from "./ui/button"
 import { X } from "lucide-react"
 import { Message } from "@/model/User"
 import { useToast } from "./ui/use-toast"
+import axios from "axios"
+import { ApiResponse } from "@/types/ApiResponse"
 
 type MessaageCardProps ={
     message: Message;
@@ -31,7 +33,15 @@ type MessaageCardProps ={
 
 function MessageCard({message,onMessageDelete}:MessaageCardProps) {
     const {toast} =useToast()
-    
+    const handleDeleteConfirm = async() => {
+      const response = await axios.delete<ApiResponse>(`/api/delete-message/${message._id}`)
+      toast({
+            title: response.data.message,
+        
+      })
+        onMessageDelete(message._id) 
+    }
+
     return ( 
         <Card>
             <CardHeader>
