91710d3136236185c2d80eebad263e82b553011a
ratnadeep2k <55341578+Ratnadeep2k@users.noreply.github.com>
2024-08-18 19:14:17 +0530

    chore: Update dashboard page component and layout files

--- src/app/(app)/dashboard_test/page.tsx
+++ src/app/(app)/dashboard_test/page.tsx
@@ -2,12 +2,25 @@
 
 import { useToast } from '@/components/ui/use-toast';
 import { Message } from '@/model/User'
+import { acceptMessageSchema } from '@/schemas/acceptMessageSchema';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useSession } from 'next-auth/react';
 import React, { useState } from 'react'
+import { useForm } from 'react-hook-form';
 
 function dashboard() {
   const [messages,setMessages] = useState<Message[]>([])
   const [loading,setIsLoading] = useState(false);
   const {toast} = useToast()
+  //optimistic UI
+
+  const handleDeleteMessage = (messageId : string)=>{
+    setMessages(messages.filter((message)=>message._id!==messageId))
+  }
+  const {data: session} = useSession()
+  const form = useForm({
+    resolver:zodResolver(acceptMessageSchema)
+  })
   return (
     <div>page</div>
   )
