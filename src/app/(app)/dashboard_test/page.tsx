d5f000acdf833e3fec3d44769829a32c63c94b0c
ratnadeep2k <55341578+Ratnadeep2k@users.noreply.github.com>
2024-08-18 19:09:28 +0530

    feat: Add @radix-ui/react-separator and @radix-ui/react-switch dependencies

--- src/app/(app)/dashboard_test/page.tsx
+++ src/app/(app)/dashboard_test/page.tsx
@@ -1,6 +1,13 @@
-import React from 'react'
+'use client'
+
+import { useToast } from '@/components/ui/use-toast';
+import { Message } from '@/model/User'
+import React, { useState } from 'react'
 
 function dashboard() {
+  const [messages,setMessages] = useState<Message[]>([])
+  const [loading,setIsLoading] = useState(false);
+  const {toast} = useToast()
   return (
     <div>page</div>
   )
